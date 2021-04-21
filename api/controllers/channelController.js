209d6460a1fc0bfc8b7a18adc6930952baa370c7
Elena <sagva2014@gmail.com>
2021-04-21 10:10:52 +0200

    Installed node-fetch

--- api/controllers/channelController.js
+++ api/controllers/channelController.js
@@ -1,5 +1,8 @@
+const sqlite3 = require("sqlite3");
+const fetch = require('node-fetch')
+
 const getAllChannels = async (req,res) => {
-    let allChannels = fetch(`http://api.sr.se/api/v2/channels/?format=json`)
+    let allChannels = await fetch(`http://api.sr.se/api/v2/channels/?format=json`)
     allChannels = await allChannels.json()
     res.json(allChannels)
 }
--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const server = express()
+const path = require("path");
 const channelRoutes = require('./routes/channelRoutes')
 const port = 3001
-const prefixRoute = 'api/v1/channels'
+const prefixRoute = '/api/v1/channels'
 
+// Server setup
+const server = express()
 
+// Make sure the server can read the req.body object
+server.use(express.json())
+
+// Routes setup
+server.use(prefixRoute, channelRoutes)
+
+// Serve static files, makes the frontend files "available" to the backend
+server.use(express.static(path.join(__dirname, "../build")));
+
+// Starts the server
 server.listen(port, (err) => {
     if(err) {
         console.error('The server couldn\'t start');
@@ -12,11 +24,4 @@ server.listen(port, (err) => {
     }
     console.log('Listening on port 8080');
 })
-
-server.use(express.json())
-
-// Serve static files, makes the frontend files "available" to the backend
-server.use(express.static(path.join(__dirname, "../build")));
-
-server.use(prefixRoute, channelRoutes)
 
