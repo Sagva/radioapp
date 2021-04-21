8df942d699a45de296b2403573ea4b12c6a17ecc
Elena <sagva2014@gmail.com>
2021-04-21 16:30:15 +0200

    Added logo and styling to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import styles from '../styles/Navbar.module.css'
+import Logo from '../images/logo.png';
 
 function Navbar () {
     return (
         <div className={styles.navbar}>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
+            <div className={styles.brandBox}>
+                <img className={styles.brandLogo} src={Logo} alt="Logo"/>
+                <span className={styles.brandName}>Radio</span>
+            </div>
+            <ul>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                {/* <NavLink to='#'>Signin</NavLink> */}
+            </ul>
         </div>
     )
 }
