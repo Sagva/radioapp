24b83cded6de66a9331b725f99794d4cd478e327
Elena <sagva2014@gmail.com>
2021-04-22 10:12:42 +0200

    Moved all channels from Home to ChannelList

---
+++ src/components/ChannelList.js
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { ChannelContext } from "../contexts/ChannelContext";
+import styles from "../styles/ChannelList.module.css";
+
+const ChannelList = () => {
+    const history = useHistory()
+  
+    const { channels, oneChannel } = useContext(ChannelContext);
+    let fetchedChannels;
+    
+   
+    let content = ''
+    if(channels) {
+        fetchedChannels = channels.channels
+        console.log(channels.channels);
+        content = <div className={styles.wrapper}>{fetchedChannels.map((ch, i) => (
+
+            <div key={i} className={styles.channelBox} onClick={() => history.push(`/channels/${ch.id}`)}>
+                <img  src={ch.image} alt={`${ch.channeltype} ${ch.name}`}></img>
+            </div>  
+        ))}
+        
+            
+        </div>
+    } else {
+        content = <div>Loading...</div>
+    }
+
+    return (
+        <div className={styles.channelList}>
+           
+            {content}
+            
+        </div>
+    );
+};
+
+export default ChannelList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,56 @@
-import { useContext } from "react";
-import { ChannelContext } from "../contexts/ChannelContext";
+
 import styles from "../styles/Home.module.css";
+import ChannelList from "../components/ChannelList";
 
 const HomePage = () => {
-  
-    const { channels } = useContext(ChannelContext);
-    let fetchedChannels;
-    let content = ''
-    if(channels) {
-        fetchedChannels = channels.channels
-        console.log(channels.channels);
-        content = <div className={styles.wrapper}>{fetchedChannels.map((ch, i) => (
-
-            <div key={i} className={styles.channelBox}><img  src={ch.image} alt={`${ch.channeltype} ${ch.name}`}></img></div>  
-        ))}</div>
-    } else {
-        content = <div>Loading...</div>
-    }
-
+    
     return (
         <div className={styles.home}>
-            <h1>Our channels</h1>
-            {content}
+            <h1>Våra kanaler</h1>
+            <ChannelList></ChannelList>
             
         </div>
     );
 };
 
 export default HomePage;
+
+// import { useContext } from "react";
+// import { useHistory } from "react-router-dom";
+// import { ChannelContext } from "../contexts/ChannelContext";
+// import styles from "../styles/Home.module.css";
+
+// const HomePage = () => {
+//     const history = useHistory()
+  
+//     const { channels, oneChannel } = useContext(ChannelContext);
+//     let fetchedChannels;
+    
+   
+//     let content = ''
+//     if(channels) {
+//         fetchedChannels = channels.channels
+//         console.log(channels.channels);
+//         content = <div className={styles.wrapper}>{fetchedChannels.map((ch, i) => (
+
+//             <div key={i} className={styles.channelBox} onClick={() => history.push(`/channels/${ch.id}`)}>
+//                 <img  src={ch.image} alt={`${ch.channeltype} ${ch.name}`}></img>
+//             </div>  
+//         ))}
+        
+            
+//         </div>
+//     } else {
+//         content = <div>Loading...</div>
+//     }
+
+//     return (
+//         <div className={styles.home}>
+//             <h1>Våra kanaler</h1>
+//             {content}
+            
+//         </div>
+//     );
+// };
+
+// export default HomePage;
